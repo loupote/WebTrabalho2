ed1f2e2b15f0bc652748f9bb44fea25a12aa2adf
Louis Pottier <louispottier44@gmail.com>
2024-11-30 13:44:02 -0300

    README added

--- frontend/typescript/script.ts
+++ frontend/typescript/script.ts
@@ -1,3 +1,9 @@
+onload = function () {
+    exibeListaDeParticipantes();   // exibe lista de participantes ao carregar a página
+    (document.getElementById('insere') as HTMLButtonElement).addEventListener('click', evento => { location.href = 'insereParticipante.html' });
+    (document.getElementById('remove') as HTMLButtonElement).addEventListener('click', apagaParticipantes);
+}
+
 function exibeListaDeParticipantes() {
     fetch(backendAddress + "participantes/lista/")
         .then(response => response.json())
@@ -5,44 +11,17 @@ function exibeListaDeParticipantes() {
             let campos = ['id', 'nome', 'sobrenome', 'distancia', 'tempo'];
             let tbody = document.getElementById('idtbody') as HTMLTableSectionElement;
             tbody.innerHTML = "";
-
-            /* novo */
-            const searchInput = (document.getElementById('searchInput') as HTMLInputElement)?.value.toLowerCase();
-
             for (let participante of participantes) {
                 let tr = document.createElement('tr') as HTMLTableRowElement;
-
-                /* novo */
-                if (!searchInput) {
-                    for (let i = 0; i < campos.length; i++) {
-                        let td = document.createElement('td') as HTMLTableCellElement;
-                        let href = document.createElement('a') as HTMLAnchorElement;
-                        href.setAttribute('href', 'update.html?id=' + participante['id']);
-                        let texto = document.createTextNode(participante[campos[i]]) as Text;
-                        href.appendChild(texto);
-                        td.appendChild(href);
-                        tr.appendChild(td);
-                    }
-                } else {
-                    let participanteEncontrado = participantes.find(p =>
-                        campos.some(campo => p[campo].toString().toLowerCase().includes(searchInput))
-                    );
-
-                    if (participanteEncontrado) {
-                        for (let i = 0; i < campos.length; i++) {
-                            let td = document.createElement('td') as HTMLTableCellElement;
-                            let href = document.createElement('a') as HTMLAnchorElement;
-                            href.setAttribute('href', 'update.html?id=' + participanteEncontrado['id']);
-                            let texto = document.createTextNode(participanteEncontrado[campos[i]]) as Text;
-                            href.appendChild(texto);
-                            td.appendChild(href);
-                            tr.appendChild(td);
-                        }
-                    } else {
-                        console.log("Aucun coureur n'a été trouvé");
-                    }
+                for (let i = 0; i < campos.length; i++) {
+                    let td = document.createElement('td') as HTMLTableCellElement;
+                    let href = document.createElement('a') as HTMLAnchorElement;
+                    href.setAttribute('href', 'update.html?id=' + participante['id']);
+                    let texto = document.createTextNode(participante[campos[i]]) as Text;
+                    href.appendChild(texto);
+                    td.appendChild(href);
+                    tr.appendChild(td);
                 }
-
                 let checkbox = document.createElement('input') as HTMLInputElement;
                 checkbox.setAttribute('type', 'checkbox');
                 checkbox.setAttribute('name', 'id');
@@ -57,4 +36,26 @@ function exibeListaDeParticipantes() {
         .catch(error => {
             console.error("Erro:", error);
         });
+}
+
+let apagaParticipantes = (evento: Event) => {
+    evento.preventDefault();
+    const checkboxes = document.querySelectorAll<HTMLInputElement>(
+        'input[type="checkbox"]:checked');
+    const checkedValues: string[] = [];
+    checkboxes.forEach(checkbox => { checkedValues.push(checkbox.value); });
+    fetch(backendAddress + "participantes/lista/", {
+        method: 'DELETE',
+        body: JSON.stringify(checkedValues),
+        headers: { 'Content-Type': 'application/json', }
+    })
+        .then(response => {
+            if (response.ok) {
+                alert('Dados removidos com sucesso');
+            } else {
+                alert('Dados removidos com erro');
+            }
+        })
+        .catch(error => { console.log(error) })
+        .finally(() => { exibeListaDeParticipantes(); })
 }
